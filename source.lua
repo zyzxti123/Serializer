16a44462803946dfa787f860e3c13e28c7b4f9c7
zyzxti <145914233+zyzxti123@users.noreply.github.com>
2024-06-29 05:48:45 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,21 +1,21 @@
 local Serializer = {}
 Serializer.__index = Serializer
 
-local Watermark = "--[["
+local Watermark: string = ""
+Watermark = Watermark .. "--[["
 Watermark = Watermark .. "\n@developer: zyzxti"
 Watermark = Watermark .. "\n@contact: zyzxti#2047"
-Watermark = Watermark .. "\n@usage: serializing tables to string / serializing json to serialized table"
-Watermark = Watermark .. "\n@version: 1.3.6f | https://github.com/zyzxti123/Serializer"
+Watermark = Watermark .. "\n@version: 1.4 | https://github.com/zyzxti123/Serializer"
 Watermark = Watermark .. "\n]]--"
 Watermark = Watermark .. string.rep("\n", 3)
 
-function Serializer:addTabSpaces(str, depth)
+function Serializer:addTabSpaces(str: string, depth: number): string
     return string.rep("\t", depth or 0) .. str
 end
 
-function Serializer:getArguments(func)
-    local args = {}
-    local funcInfo = debug.getinfo(func)
+function Serializer:getArguments(func): {string}
+    local args: {string} = {}
+    local funcInfo: {} = debug.getinfo(func)
 
     if funcInfo.nups then
         for i = 1, funcInfo.nups do
@@ -30,15 +30,61 @@ function Serializer:getArguments(func)
     return args
 end
 
-function Serializer:formatArguments(args)
+function Serializer:formatArguments(args): string
     return args and #args > 0 and table.concat(args, ", ") or ""
 end
 
-function Serializer:serializeFunction(func, depth)
-    local args = self:getArguments(func)
-    local formattedArgs = self:formatArguments(args)
+--TODO: add more cool formating for values :)
+function Serializer:formatValue(val: any): string
+    local valueType: any = typeof(val)
 
-    local output = "function(" .. formattedArgs .. ")"
+    if valueType == "boolean" or valueType == "number" or valueType == "string" then
+        return val
+    elseif valueType == "CFrame" then
+        return string.format("CFrame.new(%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)", val:GetComponents())
+    elseif valueType == "Vector3" then
+        return string.format("Vector3.new(%f, %f, %f)", val.X, val.Y, val.Z)
+    elseif valueType == "Vector2" then 
+        return string.format("Vector2.new(%f, %f)", val.X, val.Y)
+    elseif valueType == "Color3" then
+        return string.format("Color3.new(%f, %f, %f)", val.R, val.G, val.B)
+    elseif valueType == "BrickColor" then
+        return string.format("BrickColor.new('%s')", val.Name)
+    elseif valueType == "UDim2" then
+        return string.format("UDim2.new(%f, %d, %f, %d)", val.X.Scale, val.X.Offset, val.Y.Scale, val.Y.Offset)
+    elseif valueType == "UDim" then
+        return string.format("UDim.new(%f, %d)", val.Scale, val.Offset)
+    elseif valueType == "EnumItem" then
+        return string.format("%s.%s", val.EnumType.Name, val.Name)
+    elseif valueType == "NumberSequence" then
+        local keypoints: {string} = {}
+
+        for _, keypoint: NumberSequenceKeypoint in ipairs(val.Keypoints) do
+            table.insert(keypoints, string.format("NumberSequenceKeypoint.new(%f, %f, %f)", keypoint.Time, keypoint.Value, keypoint.Envelope))
+        end
+
+        return string.format("NumberSequence.new({%s})", table.concat(keypoints, ", "))
+    else
+        --warn(valueType, "is not supported by self:formatValues(...)")
+        return string.format("%q", tostring(val))
+    end
+end
+
+function Serializer:removeEmptyStrings(tbl: {string}): {string}
+    local result: {string} = {}
+    for _, val: string in ipairs(tbl) do
+        if val ~= "" --[[and val ~= "\n"]] then
+            table.insert(result, val)
+        end
+    end
+    return result
+end
+
+function Serializer:serializeFunction(func: any, depth: number): string
+    local args: {string} = self:getArguments(func)
+    local formattedArgs: string = self:formatArguments(args)
+
+    local output: string = "function(" .. formattedArgs .. ")"
           
     if self.options.DebugFunctions then
         local getconstants = debug.getconstants
@@ -86,14 +132,14 @@ function Serializer:serializeFunction(func, depth)
     return output
 end
 
-function Serializer:serializeTable(input, depth)
+function Serializer:serializeTable(input: {}, depth: number): string
     local output = {}
     depth = depth or 0
 
-    for key, value in next, input do
-        local keyStr = string.format("[%s]", typeof(key) == "number" and tostring(key) or "'" .. tostring(key) .. "'")
-        local valueType = type(value)
-        local formattedStr = self:addTabSpaces(keyStr .. " = ", depth)
+    for key: any, value: any in next, input do
+        local keyStr: string = string.format("[%s]", typeof(key) == "number" and tostring(key) or "'" .. tostring(key) .. "'")
+        local valueType: any = typeof(value)
+        local formattedStr: string = self:addTabSpaces(keyStr .. " = ", depth)
 
         if valueType == "table" then
             formattedStr = formattedStr
@@ -108,7 +154,7 @@ function Serializer:serializeTable(input, depth)
                            .. ","
         else
             formattedStr = formattedStr
-                           .. string.format("%q", tostring(value))
+                           .. self:serializeValue(value)--string.format("%q", tostring(value))
                            .. "," 
         end
 
@@ -123,14 +169,7 @@ function Serializer:serializeTable(input, depth)
         table.insert(output, formattedStr)
     end
 
-    --//Removing dead spaces
-    for i, v in next, output do
-        if v == "" or v == "\n" then
-            table.remove(output, i)
-        end
-    end
-
-    return table.concat(output, "\n")
+    return Watermark .. table.concat(self:removeEmptyStrings(output), "\n")
 end
 
 export type Options = {
@@ -153,14 +192,12 @@ return function(options: Options?)
             end)
     
             assert(success, "The first argument in serializeJSON must be a JSON!")
-    
-            return Watermark .. "\nreturn {\n" .. self:serializeTable(result, 1) .. "\n}"
+            return"\nreturn {\n" .. self:serializeTable(result, 1) .. "\n}"
         end,
     
         serializeTable = function(input)
             assert(typeof(input) == "table", "The first argument in serializeTable must be a Table!")
-    
-            return Watermark .. "\nreturn {\n" .. self:serializeTable(input, 1) .. "\n}"
+            return "\nreturn {\n" .. self:serializeTable(input, 1) .. "\n}"
         end
     }
 end
